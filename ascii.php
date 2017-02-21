1ca63b85e85dbf8711b5ff8c84b22dfc0d63e10b
Abdelffatteh Chebil <abdelffatteh.chebil@fullsix.com>
2017-02-21 22:50:50 +0100

    script display wording

--- ascii.php
+++ ascii.php
@@ -1,9 +1,59 @@
 #!/usr/bin/php -q
 <?php
 
-if(isset($argv[1]) && strlen($argv[1])) {
-	$string = $argv[1];
-}else {
-	echo 'Please Specify a String';
-	exit(1);
+require 'Matrice.php';
+
+Class convertStringToAscii
+{
+    public $arrayOfString =[];
+
+
+    function __construct()
+    {
+        global $argv;
+        if(isset($argv[1]) && strlen($argv[1]) && preg_match('/^[A-Z0-9 .\-]+$/i', $argv[1])) {
+            $this->arrayOfString = str_split($argv[1]);
+        }else {
+            echo 'Please Specify a String';
+            exit(1);
+        }
+    }
+
+    public function transformletterToMAtrice() {
+
+        $displaystring = [];
+        $matrice = new Matrice();
+
+        for($i = 0 ;$i < count($this->arrayOfString);$i++) {
+            $current_letter = $this->arrayOfString[$i];
+            $matricelet = $matrice->matrice_letters[$current_letter];
+            foreach ($matricelet as $key => $line) {
+                if(isset($displaystring[$key])){
+                    $displaystring[$key] = array_merge($displaystring[$key],$line);
+                }else {
+                    $displaystring[$key] = $line;
+                }
+
+            }
+        }
+
+        return $displaystring;
+    }
+
+    public function display() {
+        $transform_string = $this->transformletterToMAtrice();
+        foreach ($transform_string as $keyline => $line) {
+            foreach ($line as $keuColumn => $column) {
+                if($column == 1){
+                    echo '#';
+                }else {
+                    echo ' ';
+                }
+            }
+            echo "\n";
+        }
+    }
 }
+
+$conv = new convertStringToAscii();
+$conv->display();
